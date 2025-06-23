55d0f14f54283164da6c83c8680594f3eb3fc74d
Sajan-Kumar-Sahu <saajansahu.3007@gmail.com>
2025-06-23 22:09:02 +0530

    corsconfig modified

--- src/main/java/com/backbenchcoders/InnSpiration/config/CorsConfig.java
+++ src/main/java/com/backbenchcoders/InnSpiration/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://localhost:5174") //
+                        .allowedOrigins("https://innspiration-booking.netlify.app", "http://localhost:5173", "http://localhost:5174") //
                         // Allow React app to access
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*")
