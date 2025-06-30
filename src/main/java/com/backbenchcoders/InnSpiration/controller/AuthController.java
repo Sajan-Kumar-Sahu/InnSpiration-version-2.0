f3111835f2cdb6b6ea50109865f0b9dbb25157d4
Sajan-Kumar-Sahu <saajansahu.3007@gmail.com>
2025-06-30 15:34:44 +0530

    Monitor endpoint created

--- src/main/java/com/backbenchcoders/InnSpiration/controller/AuthController.java
+++ src/main/java/com/backbenchcoders/InnSpiration/controller/AuthController.java
@@ -13,10 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationServiceException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 
@@ -78,5 +75,11 @@ public class AuthController {
         String accessToken = authService.refreshToken(refreshToken);
         return ResponseEntity.ok(new LoginResponseDto(accessToken));
     }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
+    }
+
 
 }
