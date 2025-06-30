ea368804e5b616e9a636ab5f5bf764dd450a2a15
Sajan-Kumar-Sahu <saajansahu.3007@gmail.com>
2025-06-30 17:02:09 +0530

    Internal Server Error Fixed

--- src/main/java/com/backbenchcoders/InnSpiration/controller/HealthController.java
+++ src/main/java/com/backbenchcoders/InnSpiration/controller/HealthController.java
@@ -1,5 +1,6 @@
 package com.backbenchcoders.InnSpiration.controller;
 
+import com.backbenchcoders.InnSpiration.advice.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
     @GetMapping("/health")
-    public ResponseEntity<String> healthCheck() {
-        return ResponseEntity.ok("OK");
+    public ResponseEntity<ApiResponse<String>> healthCheck() {
+        ApiResponse<String> response = new ApiResponse<>("OK");
+        return ResponseEntity.ok(response);
     }
 }
