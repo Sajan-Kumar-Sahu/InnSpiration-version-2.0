0c19bcf0a8d9de7055930fd8c2fbdf819c2f4b7e
Sajan-Kumar-Sahu <saajansahu.3007@gmail.com>
2025-08-06 10:05:49 +0530

    health updated

--- src/main/java/com/backbenchcoders/InnSpiration/controller/HealthController.java
+++ src/main/java/com/backbenchcoders/InnSpiration/controller/HealthController.java
@@ -1,26 +1,15 @@
 package com.backbenchcoders.InnSpiration.controller;
 
 import com.backbenchcoders.InnSpiration.advice.ApiResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
     @GetMapping("/health")
     public ResponseEntity<ApiResponse<String>> healthCheck() {
-        try {
-            jdbcTemplate.execute("SELECT 1");
-            return ResponseEntity.ok(new ApiResponse<>("OK - DB Connected ✅"));
-        } catch (Exception e) {
-            return ResponseEntity.status(500)
-                    .body(new ApiResponse<>("ERROR - DB Not Reachable ❌: " + e.getMessage()));
-        }
+        return ResponseEntity.ok(new ApiResponse<>("OK - App is running 🚀"));
     }
 }
